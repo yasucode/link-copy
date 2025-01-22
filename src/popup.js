bed725ee63798060f0996da0040826de181f9b75
yaffy <p07108@gmail.com>
2025-01-22 11:37:19 +0900

    popup windowが自動で閉じるように修正

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,13 @@
 document.getElementById("copyPlainText").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.runtime.sendMessage({ action: "copyPlainText", tab: tabs[0].id });
+    chrome.runtime.sendMessage({ action: "copyPlainText", tab: tabs[0] });
+    window.close();
   });
 });
 
 document.getElementById("copyMarkdown").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.runtime.sendMessage({ action: "copyMarkdown", tab: tabs[0].id });
+    chrome.runtime.sendMessage({ action: "copyMarkdown", tab: tabs[0] });
+    window.close();
   });
 });
